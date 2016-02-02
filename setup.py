226d25a01ddfd5e4ff02631b5bfe78caa4aad2ad
lijiuyang <lijiuyang@bytedance.com>
2016-02-02 20:34:32 +0800

    offer use StatusManager

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+        name='pydq',
+        version='0.0.1',
+        keywords=('data', 'query'),
+        description='a data query for python',
+        license='Apache 2.0',
+        install_requires=[],
+        author='Pyiner',
+        author_email='pyiner.io@gmail.com',
+        packages=find_packages(),
+        platforms='any',
+)
